366e022ecc7572fd30ad605a08231a0a503bc4a9
mjshin0425 <junis0425@gmail.com>
2023-12-20 00:04:19 +0900

    [UPDATE] dino 가 오른쪽으로 이동하는 애니메이션 추가

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ canvas.height = window.innerHeight - 100;
 // ctx.fillStyle = "green";
 // ctx.fillRect(10, 10, 100, 100);
 
-// 공룡 등장 좌표
+// 1.공룡 등장 좌표
 const dino = {
   x: 10,
   y: 200,
@@ -19,18 +19,19 @@ const dino = {
   },
 };
 
+// 3.애니메이션
+dino.x += 1;
 dino.draw();
 
-// 장애물 생성
+// 2.장애물 생성
 
 class Cactus {
   constructor() {
-    this.x = 500;
+    this.x = 100;
     this.y = 200;
     this.width = 50;
     this.height = 50;
   }
-
   draw() {
     ctx.fillStyle = "red";
     ctx.fillRect(this.x, this.y, this.width, this.height);
@@ -39,3 +40,11 @@ class Cactus {
 
 var cactus = new Cactus();
 cactus.draw();
+
+function 프레임마다실행할거() {
+  requestAnimationFrame(프레임마다실행할거);
+  dino.x++;
+  dino.draw();
+}
+
+프레임마다실행할거();
