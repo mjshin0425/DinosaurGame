3d16ed4f49cb474eebe6d040e0f803fbb532e9ab
mjshin0425 <junis0425@gmail.com>
2023-12-19 23:18:53 +0900

    [UPDATE] 공룡 등장 좌표 설정

--- main.js
+++ main.js
@@ -4,5 +4,19 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth - 100;
 canvas.height = window.innerHeight - 100;
 
-ctx.fillstyle = "green";
-ctx.fillRect(10, 10, 100, 100);
+// ctx.fillStyle = "green";
+// ctx.fillRect(10, 10, 100, 100);
+
+// 공룡 등장 좌표
+const dino = {
+  x: 10,
+  y: 200,
+  width: 50,
+  height: 50,
+  draw() {
+    ctx.fillStyle = "green";
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  },
+};
+
+dino.draw();
